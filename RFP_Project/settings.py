f0414301ae0aff05c1d6083c16b24fe1bb0e3e4b
Dian Vasilev <dian.vasilev02@gmail.com>
2024-02-09 16:22:37 +0200

    start friend logic

--- RFP_Project/settings.py
+++ RFP_Project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'friend.apps.FriendConfig',
     'users.apps.UsersConfig',
     'mainpage.apps.MainpageConfig',
     'django.contrib.admin',
